d3a3f61a1c475272aa4f00763f94e3eddd190872
Apple <apple@Apples-MacBook-Pro-2.local>
2019-07-11 15:33:03 -0500

    Emojional Iteraion 1

--- ViewController.swift
+++ ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessage(sender: UIButton) {
+        
+        //After button is tapped, the message displays.
+        //There will be multiple messages.
+        
+        let alertController = UIAlertController(title: "Genre", message: "Tongue Tied by Grouplove", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+
+    }
 
 }
 
